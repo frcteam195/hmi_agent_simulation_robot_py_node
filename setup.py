b3e6864611bf386c480423590eb5c1606992834a
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-12-09 20:27:38 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['hmi_agent_simulation_robot_py_node'],  #packages=['hmi_agent_simulation_robot_py_node', 'hmi_agent_simulation_robot_py_node.subnode'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
